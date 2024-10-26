32eeef05e2a5420a4f6e1b84555dfa70e984d8ac
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-10-26 13:20:56 +0200

    Update Contact_Us_Steps.java

--- Contact_Us_Steps.java
+++ Contact_Us_Steps.java
@@ -9,8 +9,10 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 
 import java.time.Duration;
 
@@ -52,21 +54,22 @@ public class Contact_Us_Steps {
 
     }
     @And("I enter an email address")
-    public void i_enter_an_email_address() throws InterruptedException {
+    public void i_enter_an_email_address() {
         driver.findElement(By.name("email")).sendKeys("boogey@mail.com");
-        Thread.sleep(3000);
 
     }
     @And("I enter a comment")
     public void i_enter_a_comment() {
+        driver.findElement(By.name("message")).sendKeys("Uuuuu I like it!");
 
     }
     @And("I click on the submit button")
     public void i_click_on_the_submit_button() {
-
+        driver.findElement(By.cssSelector("[value=\"SUBMIT\"]")).click();
     }
     @Then("I should be presented with a succesful contact us submission message")
     public void i_should_be_presented_with_a_succesful_contact_us_submission_message() {
-
+        WebElement contactUs_Submission_Message = driver.findElement(By.xpath("//div[@id='contact_reply']/h1"));
+        Assert.assertEquals(contactUs_Submission_Message.getText(), "Thank You for your Message!");
     }
 }
